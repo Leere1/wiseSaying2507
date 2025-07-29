b7f2ee6e6616014c01cc21a78532eeb036a15962
leere1 <dlfhrdms@gmail.com>
2025-07-29 16:20:41 +0900

    step4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,43 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+        System.out.println("==명언 앱==");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        String cmd = "명령) ";
+        String regist = "명언 : ";
+        String author = "작가 : ";
+
+        String viewStr = cmd;
+
+        int num = 0;
+
+        while(true) {
+            System.out.print(viewStr);
+            String inputValue = sc.nextLine().trim();
+
+            if (inputValue.equals("종료") || inputValue.equals("exit")) {
+                System.out.println("프로그램을 종료합니다");
+                break;
+            }
+            if(inputValue.equals("등록")) {
+                viewStr = regist;
+            } else if(viewStr.equals(regist)) {
+                viewStr = author;
+                num++;
+            } else if(viewStr.equals(author)) {
+                viewStr = cmd;
+                System.out.println(num + " 번 명언이 등록되었습니다.");
+            }
         }
+
+        sc.close();
+
     }
 }
