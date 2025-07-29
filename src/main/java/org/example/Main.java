7ccef3eaed88727a5c6f474b83e938c09f165aae
leere1 <dlfhrdms@gmail.com>
2025-07-29 17:44:54 +0900

    step6,7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,9 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
@@ -18,7 +19,8 @@ public class Main {
         String viewText = "명령) ";
 
 
-        List<wiseSaying> sayings = new ArrayList<>();
+        Map<Integer, wiseSaying> sayings = new TreeMap<>(Comparator.reverseOrder());
+        int id = 0;
 
         while(true) {
             System.out.print(viewText);
@@ -31,26 +33,35 @@ public class Main {
 
             if(inputValue.equals("등록")) {   // 다음텍스트는 명언 :
                 viewText = "명언 : ";
+            }else if(inputValue.equals("목록")) {
+                viewText = "번호 / 작가 / 명언 \n----------------------\n";
+
+                for (wiseSaying value : sayings.values()) {
+                    viewText += value.toString();
+                    viewText += "\n";
+                }
+                System.out.println(viewText);
+                viewText = "명령) ";
+            }  else if(inputValue.contains("삭제?id=")) {
+                int deleteId = Integer.parseInt(inputValue.substring(6));
+                if(sayings.get(deleteId) != null) {
+                    sayings.remove(deleteId);
+                    System.out.println(deleteId + "번 명언이 삭제되었습니다.");
+                } else {
+                    System.out.println(deleteId + "번 명언은 존재하지 않습니다.");
+                }
+                viewText = "명령) ";
             } else if(viewText.equals("명언 : ")) { // 다음 텍스트는 작가 :
                 text = inputValue;
                 viewText = "작가 : ";
             } else if(viewText.equals("작가 : ")) { // 다음텍스트는 명령)
                 viewText = "명령) ";
                 author = inputValue;
-                int id = sayings.size() +1;
-                sayings.add(0, new wiseSaying(id, text, author));
+                id++;
+                sayings.put(id, new wiseSaying(id, text, author));
                 text = "";
                 author = "";
                 System.out.println(id + "번 명언이 등록되었습니다.");
-            } else if(inputValue.equals("목록")) {
-                viewText = "번호 / 작가 / 명언 \n----------------------\n";
-
-                for (wiseSaying saying : sayings) {
-                    viewText += saying.toString();
-                    viewText += "\n";
-                }
-                System.out.println(viewText);
-                viewText = "명령) ";
             }
         }
 
